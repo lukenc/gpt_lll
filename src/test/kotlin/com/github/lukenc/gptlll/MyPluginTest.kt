191a5e70c0be781a1c13edd133cdf990f33ddd3f
GitHub Action <action@github.com>
2023-06-01 16:23:20 +0000

    Template cleanup

--- src/test/kotlin/com/github/lukenc/gptlll/MyPluginTest.kt
+++ src/test/kotlin/com/github/lukenc/gptlll/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lukenc.gptlll
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lukenc.gptlll.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
